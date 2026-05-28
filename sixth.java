49acc2d9c8145270a44697bad7939b40b378f805
Ahmet Burak Can <118999270+burakshaus@users.noreply.github.com>
2026-05-28 21:28:45 +0300

    daily commit

---
+++ sixth.java
@@ -0,0 +1,9 @@
+public class sixth {
+    public static void main(String[] args) {
+        System.out.println("Qadric power of numbers from 1 to 9:");
+        for (int  i = 1; i <10; i++) {
+            System.out.print(i+ " ");
+            System.out.println(i*i*i);
+        }
+    }
+}
