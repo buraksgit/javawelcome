83f776f8fffcda8ae71e31fb9dbc2b091e70155f
Ahmet Burak Can <118999270+burakshaus@users.noreply.github.com>
2026-05-28 21:27:32 +0300

    daily commit

--- fifth.java
+++ fifth.java
@@ -2,7 +2,7 @@ public class fifth {
     public static void main(String[] args) {
         System.out.println("Squares of numbers from 1 to 9:");
         for (int  i = 1; i <10; i++) {
-            System.out.print(i + " ");
+            System.out.print(i+ " ");
 System.out.println(i*i);
 
         }
