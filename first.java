bcb9cc147b0022dee44c80babbfaf20eba5ee0de
Ahmet Burak Can <118999270+burakshaus@users.noreply.github.com>
2026-05-28 15:36:39 +0300

    daily commit

--- first.java
+++ first.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        int age = input.nextInt();
+        System.out.println(age);
     }
 }
