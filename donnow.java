5b1f7539ef90563a9cb63298e85818580fccd02f
Ahmet Burak Can <118999270+burakshaus@users.noreply.github.com>
2026-06-01 17:20:39 +0300

    daily commdaily commitit

---
+++ donnow.java
@@ -0,0 +1,9 @@
+public class donnow {
+    public static void main(String[] args) {
+        System.out.println("Qadric power of numbers from 1 to 9:");
+        for (int  i = 1; i <10; i++) {
+            System.out.print(i+ " ");
+            System.out.println(i*i);
+        }
+    }
+}
